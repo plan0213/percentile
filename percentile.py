2941346b81cd35b11ff68f8635203a2d3be15c69
Yuya Niimi <yuu.niimi@gmail.com>
2016-02-08 22:58:17 +0900

    added percentile.py and first commit.

---
+++ percentile.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+
+## percentile.py
+
+
+def main():
+
+    list = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+    print calc_percentile(list, 0.95)
+
+
+def calc_percentile(list, p):
+    list.sort(key = float)
+
+    length = len(list)
+    rank = ((length - 1) * p)
+    d = rank - int(rank)
+
+    result = list[int(rank)] + (list[int(rank + 1)] - list[int(rank)]) * d
+
+    return result
+
+if __name__ == "__main__":
+
+    main()
